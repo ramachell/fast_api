e5674b818faa551b4aeba0b320d671c9fc1640a9
ramachell <mjh1548@gmail.com>
2024-01-23 23:11:14 +0900

    close,yiled 설명

--- app/__init__.py
+++ app/__init__.py
@@ -10,23 +10,6 @@ load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-# url = urlparse(DATABASE_URL)
-
-# DATABASE_URL=mysql+pymysql://user:password@localhost:33306/company
-
-# 연결 설정
-# connection = pymysql.connect(
-#     host=url.hostname,
-#     user=url.username,
-#     password=url.password,
-#     database=url.path[1:],
-#     port=url.port
-# )
-print("----------------------")
-print("----------------------")
-# print(connection)
-print("----------------------")
-print("----------------------")
 engine =create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit = False, autoflush = False, bind = engine)
 
--- app/crud.py
+++ app/crud.py
@@ -10,6 +10,13 @@ def get_db():
     finally:
         db.close()
 
+    # close() 그냥 쓰고나서 닫아주는작업 안닫으면 계속 메모리에 남아있고 메모리낭비임
+        
+    # yield : 리스트? 의 개념 제너레이트
+    # return 을 하게되면 결과를 전부 한번에 저장해놓고 쓰기때문에 메모리 낭비, 아까움
+    # yield 를 쓰게되면 한번에 1개씩 불러올수가있어서 메모리저장 측면에서도 데이터 연산 속도에서도 이득
+    # 만약 처리하는데 1초가 걸리는 연산을 20번 실행할시 return 을 하면 20초가 걸리고결과가 나오지만, yield 로 하게되면 데이터를 불러올떄 1초의 연산을 하고 데이터를 불러옴 
+
 def get_todo_items(db: Session):
     return db.query(ToDoItem).all()
 
